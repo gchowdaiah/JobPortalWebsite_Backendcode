bc83ff6dcc05d5f1d6fb0f8dec88d31c99853819
gchowdaiah <gchowdaiah223@gmail.com>
2025-05-20 15:25:28 +0530

    New Code
    New code

---
+++ JobPortalWebsiteApplication/Models/JobRole.cs
@@ -0,0 +1,10 @@
+﻿namespace JobPortalWebsite.Models
+{
+    public class JobRole
+    {
+        public string CompanyName { get; set; }
+        public string Role { get; set; }
+        public List<string> Experience { get; set; }
+        public List<string> Skills { get; set; }
+    }
+}
